06e2931449faeb15782f636eb6429d0f20f03741
clardeur <clem.lardeur@gmail.com>
2013-05-19 00:21:42 +0200

    fix: grunt build process clean: js formating

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('karmaGeneratorApp')
-  .controller('MainCtrl', function ($scope, $log) {
-  	$scope.url='';
+  .controller('MainCtrl', function ($scope) {
+    $scope.url='';
 
-  	$scope.go = function() {
-  		$scope.iframe = $scope.url;
-  	};
+    $scope.go = function() {
+      $scope.iframe = $scope.url;
+    };
   });
