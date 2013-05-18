23c5b3c2dd28bf6456b7ecd79571d69e0a199197
clardeur <clem.lardeur@gmail.com>
2013-05-18 19:20:36 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('karmaE2eGeneratorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
